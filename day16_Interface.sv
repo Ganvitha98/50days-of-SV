371a9e8e12f31bcd359c2076f95e62f7c1013206
Ganvitha98 <139239585+Ganvitha98@users.noreply.github.com>
2024-12-19 16:24:07 +0530

    Update day16_Interface.sv

--- day16_Interface.sv
+++ day16_Interface.sv
@@ -1,25 +1,27 @@
-interface interface_with_modports_and_clocking (input logic clk);
+interface ex_interface;
     logic [7:0] data;
     logic valid;
     logic ready;
 
-    // Clocking block for synchronization
-    clocking cb @(posedge clk);
-        input ready;
-        input valid;
-        output data;
-    endclocking
-
-    // Modports for producer and consumer
-    modport producer (
-        input cb.ready,
-        output cb.data,
-        output cb.valid
-    );
-
-    modport consumer (
-        input cb.data,
-        input cb.valid,
-        output cb.ready
-    );
+    modport producer (output data, valid, input ready);
+    modport consumer (input data, valid, output ready);
 endinterface
+//producer module
+module producer(simple_interface.producer intf);
+    always_ff @(posedge intf.ready) begin
+        intf.data <= $random % 256;
+        intf.valid <= 1'b1;
+        $display("Producer: Sent Data = %h", intf.data);
+    end
+endmodule
+//consumer module
+module consumer(simple_interface.consumer intf);
+    always_ff @(posedge intf.valid) begin
+        if (intf.valid) begin
+            $display("Consumer: Received Data = %h", intf.data);
+            intf.ready <= 1'b1;
+        end else begin
+            intf.ready <= 1'b0;
+        end
+    end
+endmodule
