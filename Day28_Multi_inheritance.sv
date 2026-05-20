8a657c10e56c486e3e8ef0b05a3fea0ce1e39feb
Ganvitha98 <139239585+Ganvitha98@users.noreply.github.com>
2026-05-20 16:34:33 +0530

    Create Day28_Multi_inheritance.sv

---
+++ Day28_Multi_inheritance.sv
@@ -0,0 +1,53 @@
+class parent_class;
+  bit [31:0] data_p;
+
+  function void disp_p();
+    $display("parent_class: Value of data = %0h", data_p);
+  endfunction
+endclass
+
+class child1_class extends parent_class;
+  bit [31:0] data_c1;
+
+  function void disp_c1();
+    $display("child1_class: Value of data = %0h", data_c1);
+  endfunction
+endclass
+
+
+class child_A_class extends child1_class;
+  bit [31:0] data_cA;
+
+  function void disp_cA();
+    $display("child_A_classs: Value of data = %0h", data_cA);
+  endfunction
+endclass
+
+class child_B_class extends child_A_class;
+  bit [31:0] data_cB;
+
+  function void disp_cB();
+    $display("child_B_class: Value of data = %0h", data_cB);
+  endfunction
+endclass
+
+module multilevel_inheritance;
+  initial begin
+    child_B_class cB;
+    cB = new();
+    cB.data_p = 2;
+    cB.data_c1 = 4;
+    cB.data_cA = 6;
+    cB.data_cB = 8;
+
+    cB.disp_p();
+    cB.disp_c1();
+    cB.disp_cA();
+    cB.disp_cB();
+  end
+endmodule
+//Output
+# parent_class: Value of data = 2
+# child1_class: Value of data = 4
+# child_A_classs: Value of data = 6
+# child_B_class: Value of data = 8
