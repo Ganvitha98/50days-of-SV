8e80c2b9e8c1bd50ac1651b0869b15ebe91cffda
Ganvitha98 <139239585+Ganvitha98@users.noreply.github.com>
2026-05-21 21:32:05 +0530

    Create Day-46-DisableFork.sv

---
+++ Day-46-DisableFork.sv
@@ -0,0 +1,34 @@
+module disable_fork;
+
+  initial begin
+    $display("Simulation starts at time %0t", $time);
+
+    fork
+      begin
+        #5;
+        $display("Task 1 completed at time %0t", $time);
+      end
+      begin
+        #10;
+        $display("Task 2 completed at time %0t", $time);
+      end
+      begin
+        #15;
+        $display("Task 3 completed at time %0t", $time);
+      end
+    join_none
+
+    #6;
+    $display("Disabling fork at time %0t", $time);
+    disable fork;
+
+    #5;
+    $display("Simulation ends at time %0t", $time);
+  end
+
+endmodule
+//Output
+# Simulation starts at time 0
+# Task 1 completed at time 5
+# Disabling fork at time 6
+# Simulation ends at time 11
